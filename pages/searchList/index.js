2cde9512485d8e95de002db44bf037c6ac8ce1e5
Trojan <konglanstudio@gmail.com>
2020-08-30 12:38:41 +0800

    0.1.4

--- pages/searchList/index.js
+++ pages/searchList/index.js
@@ -1,23 +1,43 @@
-// pages/searchList/index.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
-   */
+//Page Object
+Page({
   data: {
-
+    searchValue:"",
+    musicValue:[],
+    musicUrl:""
   },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
+  //options(Object)
+  onLoad: function(options){
+    this.setData({
+      searchValue:options.value
+    })
+    wx.request({
+      url:'https://autumnfish.cn/search?',
+      data:{keywords:options.value},
+      success:(result) => {
+        this.setData({
+          musicValue:result.data.result.songs
+        })
+        console.log(result.data.result.songs)
+      }
+    })
+  },
+   tapMusic(mId){
+    let musicId = mId.currentTarget.dataset.id;
+    wx.request({
+      url:'https://autumnfish.cn/song/url?',
+      data:{id:musicId},
+      success:(result) => {
+        console.log(result.data.data[0].url);
+        this.setData({
+          musicUrl:result.data.data[0].url
+        })
+        // console.log(result.data.result.songs)
+      }
+    })
+    console.log(this.musicUrl)
+  },
+  //item(index,pagePath,text)
+  onTabItemTap:function(item){
 
   }
-})
+});
